9a5ef29072fedb8a4b667deddcac676f2736b51c
DEVESH TIWARI <deveshups@gmail.com>
2026-05-10 03:26:43 +0530

    Google Authentication In Login Page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -6,6 +6,8 @@ import { IoEye } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
 import { authDataContext } from "../context/AuthContext";
 import axios from "axios";
+import { signInWithPopup } from "firebase/auth";
+import { auth, provider } from "../../utils/Firebase";
 
 const Login = () => {
   const [show, setShow] = useState(false);
@@ -27,6 +29,25 @@ const Login = () => {
       console.log(error);
     }
   };
+
+  const googleSignup = async (req, res) => {
+    try {
+      let response = await signInWithPopup(auth, provider);
+      let user = response.user;
+      let name = user.displayName;
+      let email = user.email;
+
+      let result = await axios.post(
+        serverUrl + "/api/auth/googlelogin",
+        { name, email },
+        { withCredentials: true },
+      );
+      console.log(result.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="w-[100vw] h-[100vh] bg-gradient-to-l from-[#141414] to-[#0c2025] text-[white] flex flex-col items-center justify-start">
       <div
@@ -51,7 +72,10 @@ const Login = () => {
           action=""
           className="w-[90%] h-[90%] flex flex-col items-center justify-start gap-[20px]"
         >
-          <div className="w-[90%] h-[50px] bg-[#42656cae] rounded-lg flex items-center justify-center gap-[10px] py-[20px] cursor-pointer">
+          <div
+            onClick={googleSignup}
+            className="w-[90%] h-[50px] bg-[#42656cae] rounded-lg flex items-center justify-center gap-[10px] py-[20px] cursor-pointer"
+          >
             <img src={google} alt="" className="w-[20px]" /> Registration with
             Google
           </div>
