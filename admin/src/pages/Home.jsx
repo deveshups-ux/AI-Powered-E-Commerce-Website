99c6e2988217e9e7e49c87912bd15321a3c3d0cb
DEVESH TIWARI <deveshups@gmail.com>
2026-06-01 12:53:39 +0530

    Admin : Navbar And Sidebar Ready In Home Page

--- admin/src/pages/Home.jsx
+++ admin/src/pages/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Nav from "../components/Nav";
+import Sidebar from "../components/Sidebar";
 
-const Home = () => {
-  return <div className="text-[30px] text-red-600">Home</div>;
-};
+function Home() {
+  return (
+    <div className="w-[100vw] h-[100vh] bg-gradient-to-l from-[#141414] to-[#0c2025] text-[white] relative">
+      <Nav />
+      <Sidebar />
+    </div>
+  );
+}
 
 export default Home;
