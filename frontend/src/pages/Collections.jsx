92705677081ce547a8434d9eeb82f130fef5a2d2
DEVESH TIWARI <deveshups@gmail.com>
2026-06-10 16:46:06 +0530

    Frontend : Collection Page - Filter And Cards Funtionality Add

--- frontend/src/pages/Collections.jsx
+++ frontend/src/pages/Collections.jsx
@@ -1,9 +1,174 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { FaChevronRight } from "react-icons/fa";
+import { FaChevronDown } from "react-icons/fa";
+import Title from "../components/Title";
+import { shopDataContext } from "../context/ShopContext";
+import Card from "../components/Card";
 
 const Collections = () => {
-  return (
-    <div>Collections</div>
-  )
-}
+  const [showFilter, setShowFilter] = useState(false);
+  const { products } = useContext(shopDataContext);
+  const [filterProduct, setFilterProduct] = useState([]);
+  const [category, setCategory] = useState([]);
+  const [subCategory, setSubCategory] = useState([]);
+  const [sortType, setSortType] = useState("Relevant");
 
-export default Collections
+  const toogleCategory = (e) => {
+    if (category.includes(e.target.value)) {
+      setCategory((prev) => prev.filter((item) => item !== e.target.value));
+    } else {
+      setCategory((prev) => [...prev, e.target.value]);
+    }
+  };
+
+  const toogleSubCategory = (e) => {
+    if (subCategory.includes(e.target.value)) {
+      setSubCategory((prev) => prev.filter((item) => item !== e.target.value));
+    } else {
+      setSubCategory((prev) => [...prev, e.target.value]);
+    }
+  };
+
+  const applyFilter = () => {
+    let productCopy = products.slice();
+    if (category.length > 0) {
+      productCopy = productCopy.filter((item) =>
+        category.includes(item.category),
+      );
+    }
+    if (subCategory.length > 0) {
+      productCopy = productCopy.filter((item) =>
+        subCategory.includes(item.subCategory),
+      );
+    }
+    setFilterProduct(productCopy);
+  };
+
+  useEffect(() => {
+    setFilterProduct(products);
+  }, [products]);
+
+  useEffect(() => {
+    applyFilter();
+  }, [category, subCategory]);
+
+  return (
+    <div className="w-[100vw] min-h-[100vh] bg-gradient-to-l from-[#141414] to-[#0c2025] flex items-start flex-col md:flex-row justify-start pt-[70px] overflow-x-hidden z-[2]">
+      <div
+        className={`md:w-[30vw] lg:w-[20vw] w-[100vw] md:min-h-[100vh] p-[20px] border-r-[1px] border-gray-400 text-[#aaf5fa] lg:fixed ${showFilter ? "h-[45vh]" : "h-[8vh]"}`}
+      >
+        <p
+          className="text-[25px] font-semibold flex gap-[5px] items-center justify-start"
+          onClick={() => setShowFilter((prev) => !prev)}
+        >
+          FILTERS
+          {!showFilter && <FaChevronRight className="text-[18px] md-hidden " />}
+          {showFilter && <FaChevronDown className="text-[18px] md:hidden " />}
+        </p>
+
+        <div
+          className={`border-[2px] border-[#dedcdc] pl-5 py-3 mt-6 rounded-md bg-slate-600  ${showFilter ? "" : "hidden"} md:block`}
+        >
+          <p className="text-[18px] text-[#f8fafa]">CATEGORIES</p>
+          <div className="w-[230px] h-[120px] flex items-start justify-center gap-[10px] flex-col">
+            <p className="flex items-center justify-center gap-[10px] text-[16px] font-light">
+              <input
+                type="checkbox"
+                value={"Men"}
+                className="w-3"
+                onChange={toogleCategory}
+              />{" "}
+              Men
+            </p>
+            <p className="flex items-center justify-center gap-[10px] text-[16px] font-light">
+              <input
+                type="checkbox"
+                value={"Women"}
+                className="w-3"
+                onChange={toogleCategory}
+              />{" "}
+              Women
+            </p>
+            <p className="flex items-center justify-center gap-[10px] text-[16px] font-light">
+              <input
+                type="checkbox"
+                value={"Kids"}
+                className="w-3"
+                onChange={toogleCategory}
+              />{" "}
+              Kids
+            </p>
+          </div>
+        </div>
+        <div
+          className={`border-[2px] border-[#dedcdc] pl-5 py-3 mt-6 rounded-md bg-slate-600  ${showFilter ? "" : "hidden"} md:block`}
+        >
+          <p className="text-[18px] text-[#f8fafa]">SUB-CATEGORIES</p>
+          <div className="w-[230px] h-[120px] flex items-start justify-center gap-[10px] flex-col">
+            <p className="flex items-center justify-center gap-[10px] text-[16px] font-light">
+              <input
+                type="checkbox"
+                value={"TopWear"}
+                className="w-3"
+                onChange={toogleSubCategory}
+              />
+              TopWear
+            </p>
+            <p className="flex items-center justify-center gap-[10px] text-[16px] font-light">
+              <input
+                type="checkbox"
+                value={"BottomWear"}
+                className="w-3"
+                onChange={toogleSubCategory}
+              />
+              BottomWear
+            </p>
+            <p className="flex items-center justify-center gap-[10px] text-[16px] font-light">
+              <input
+                type="checkbox"
+                value={"Winter"}
+                className="w-3"
+                onChange={toogleSubCategory}
+              />{" "}
+              Winter
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="lg:pl-[20%] md:py-[10px]">
+        <div className="md:w-[80vw] w-[100vw] p-[20px] flex justify-between flex-col lg:flex-row lg:px-[50px]">
+          <Title text1={"ALL"} text2={"COLLECTIONS"} />
+
+          <select
+            name=""
+            id=""
+            className="bg-slate-600 w-[60%] md:w-[200px] h-[50px] px-[10px] text-[white] rounded-lg hover:border-[#46d1f7] border-[2px]"
+          >
+            <option value="relavent" className="w-[100%] h-[100%]">
+              Relevant
+            </option>
+            <option value="low-high" className="w-[100%] h-[100%]">
+              Price: Low to High
+            </option>
+            <option value="high-low" className="w-[100%] h-[100%]">
+              Price: High to Low
+            </option>
+          </select>
+        </div>
+        <div className="lg:w-[80vw] md:w-[60vw] w-[100vw] min-h-[70vh] flex items-center justify-center flex-wrap gap-[30px]">
+          {filterProduct.map((item, index) => (
+            <Card
+              key={index}
+              id={item._id}
+              name={item.name}
+              price={item.price}
+              image={item.image1}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Collections;
